44c3569156b40818c0366ddc91bcf199ff5e40f6
eduardo88tafur <98570854+eduardo88tafur@users.noreply.github.com>
2022-04-18 22:12:52 -0500

    tafur creo el componente de paciente, cap 62

--- src/components/ListadoPascientes.jsx
+++ src/components/ListadoPascientes.jsx
@@ -1,9 +1,18 @@
-
+import Paciente from "./Paciente"
 
 const ListadoPascientes = () => {
   return (
-    <div className="w-1/2 lg:w-3/5">
-    <h2>Listado Pacientes</h2>
+    <div className=" md:w-1/2 lg:w-3/5  md:h-screen  overflow-y-scroll">
+    <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+    <p className="font-black text-3xl text-center">
+      administra tus {" "}
+      <span className="text-indigo-600 ">pacientes y citas</span>
+    </p>
+    <Paciente/>
+    <Paciente/>
+    <Paciente/>
+    <Paciente/>
+    <Paciente/>
     </div>
   )
 }
---
+++ src/components/Paciente.jsx
@@ -0,0 +1,26 @@
+const Paciente = () => {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10  rounded-xl">
+      <p className="font-bold mb-3 text-gray-700 uppercase"> nombre: {""}
+        <span className="font-normal normal-case">hook</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase"> propietario: {""}
+        <span className="font-normal normal-case">tafur</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase"> email: {""}
+        <span className="font-normal normal-case">correo@correo.com</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase"> fecha alta: {""}
+        <span className="font-normal normal-case">10 diciembre 2022</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase"> sintomas: {""}
+        <span className="font-normal normal-case">el perrito se murio </span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase"> nombre: {""}
+        <span className="font-normal normal-case">hook</span>
+      </p>
+    </div>
+  )
+}
+
+export default Paciente
